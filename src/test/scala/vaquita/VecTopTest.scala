d49fd3f34f533b891c30fb13311057783516ab0b
Hafsa-shoaib989 <hafsashoaib989@gmail.com>
2025-05-07 04:12:52 +0500

    chnages in build.sbt

--- src/test/scala/vaquita/VecTopTest.scala
+++ src/test/scala/vaquita/VecTopTest.scala
@@ -1,12 +1,13 @@
 package vaquita
 import chisel3._
 import chisel3.tester._
-import org.scalatest.FreeSpec
+// import org.scalatest.FreeSpec
+import org.scalatest.freespec.AnyFreeSpec
 import chisel3.experimental.BundleLiterals._
 import vaquita.configparameter.VaquitaConfig
 
 
-class VecTopTest extends FreeSpec with ChiselScalatestTester {
+class VecTopTest extends AnyFreeSpec with ChiselScalatestTester {
   "vec top test" in {
     implicit val config = new VaquitaConfig (256,32,32,8)
     test(new VaquitaTop) { dut =>
